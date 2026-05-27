e048b173e7532a7770935ed53a0d86417d65a5b8
iamusuff <myousuf438558@gmail.com>
2026-05-27 22:57:20 +0500

    hopswork login

--- src/feature_pipeline.py
+++ src/feature_pipeline.py
@@ -50,6 +50,7 @@ def connect_to_hopsworks():
         
         logger.info("Connecting to Hopsworks...")
         project = hopsworks.login(
+            host="eu-west.cloud.hopsworks.ai",
             api_key_value=HOPSWORKS_API_KEY,
             project=HOPSWORKS_PROJECT_NAME
         )
